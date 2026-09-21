ef05af740534484ea3ebb2cb9ccf4e717be2ac7d
Luiz Diego <luiz.1324diego@gmail.com>
2026-09-21 18:29:44 -0300

    first commit

---
+++ src/test/java/com/luiz/projeto_web_services/ProjetoWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz.projeto_web_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
