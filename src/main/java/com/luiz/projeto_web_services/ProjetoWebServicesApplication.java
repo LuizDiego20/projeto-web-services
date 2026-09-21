3f7798f6c90dc315cd0816030d73bcb953ff7994
Luiz Diego <luiz.1324diego@gmail.com>
2026-09-21 20:10:21 -0300

    User entity and resource

--- src/main/java/com/luiz/projeto_web_services/ProjetoWebServicesApplication.java
+++ src/main/java/com/luiz/projeto_web_services/ProjetoWebServicesApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjetoWebServicesApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetoWebServicesApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoWebServicesApplication.class, args);
+    }
 }
